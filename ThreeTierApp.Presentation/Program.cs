10ebbda237049af983dbdc38c0486f1f4f68f07c
Erik Forsgren <crapmannen@hotmail.com>
2025-10-28 11:06:13 +0100

    Lade till metod GimmeMoney

--- ThreeTierApp.Presentation/Program.cs
+++ ThreeTierApp.Presentation/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var dataManager = new ThreeTierApp.Data.DataClass();
+
+            dataManager.GimmeMoney();
         }
     }
 }
