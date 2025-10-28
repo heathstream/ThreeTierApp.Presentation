4dfcb1b10fa19813c7b080b367fd57144128f3bd
Anders Hedström <toastwaffle@gmail.com>
2025-10-28 10:37:17 +0100

    Add project files.

---
+++ ThreeTierApp.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThreeTierApp.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
